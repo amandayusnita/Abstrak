c9202258997d58a347d1ded9582b9393869a26fd
amandayusnita <115141163+amandayusnita@users.noreply.github.com>
2023-03-17 11:52:15 +0700

    Add files via upload

---
+++ JavaApplication4/src/KelasAbstract.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+class KelasAbstract {
+    public static void main(String [] args){
+        hewan h = new hewan();
+        burung b = new burung();
+        h.berjalan();
+        h.bernafas();
+        b.berjalan();
+        b.bernafas();
+    }
+    
+}
